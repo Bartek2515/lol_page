6d7949f769d369bd0cc82f37b9110532bf4b280a
Bartłomiej Rokowski <roki7-10@wp.pl>
2025-04-03 14:29:29 +0200

    Revert "stimulus"
    This reverts commit 935d3768f7102424b063bdab0d5438299f4abf5e.

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
